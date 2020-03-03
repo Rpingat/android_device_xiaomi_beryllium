c450d19ec05863e796bff61e91d514081b4d2040
PainKiller3 <ninadpatil100@gmail.com>
2020-03-03 09:13:22 +0530

    beryllium: Update Fingerprint To Coral March Security Patch
    Change-Id: I87eac6dc6968554b1951accb8f1472d2fd749ea1
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- aim_beryllium.mk
+++ aim_beryllium.mk
@@ -16,7 +16,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ1B.200205.002/6084387:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.4.0.QEJMIXM release-keys" \
